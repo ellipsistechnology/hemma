7119c6ddd6da84d304f7bab51268c5c33fc19d83
bmillar <ben.millar@gmail.com>
2017-04-30 14:18:09 +1000

    Slight change to scenario and parameters.

--- src/test/java/ellipsis/hemma/test/testcases/TestCase002_8Bus.java
+++ src/test/java/ellipsis/hemma/test/testcases/TestCase002_8Bus.java
@@ -11,7 +11,7 @@ public class TestCase002_8Bus extends TestCase
 {
 	public static void main(String[] args)
 	{
-		new TestCase002_8Bus().run(10000);
+		new TestCase002_8Bus().run(2000);
 	}
 
 	/**
@@ -22,9 +22,9 @@ public class TestCase002_8Bus extends TestCase
 	{
 		NetworkBuilder builder = new NetworkBuilder();
 		
-		builder.makeVCAgent("VC1", 200.0)
+		builder.makeVCAgent("VC1", 250.0)
 		       .setGrounded(true);
-		builder.makeVCAgent("VC2", 200.0);
+		builder.makeVCAgent("VC2", 170.0);
 		
         builder.makeCPAgents("CP", 4, -50.0);
         builder.makeCCAgents("CC", 2, -6.0);
@@ -41,20 +41,18 @@ public class TestCase002_8Bus extends TestCase
         Set<Agent> agents = builder.agentSet();
         
         // Set optimization parameters:
-        /* Best from tuning:
-			initial alpha      = 7.3978384550794E-4
-			alpha multiplier   = 1.000001
-			lambda multiplier  = 1.1855488320556264
-			initial epsilon    = 10.208361642475733
-			epsilon multiplier = 0.9833867364998466
-         */
-        setEach(agents, Agent::setAlpha,             1e-0);
-        setEach(agents, Agent::setAlphaMax,          1e24);
-        setEach(agents, Agent::setAlphaMultiplier,   1.001);
-        setEach(agents, Agent::setLambdaMax,         1000.0);
-        setEach(agents, Agent::setLambdaMultiplier,  4e-3);
-        setEach(agents, Agent::setEpsilon,           1e-6);
-        setEach(agents, Agent::setEpsilonMultiplier, 1.0);//0.999);
+        /* 1: {0.7437170678; 1.0022891615; 0.0880506421; 0.000001; 0.999999} => -0.45799673392666035 */
+        /* 2: {1.0335558193; 1.0032247029; 0.0516760301; 0.000001; 0.9919932057} => -0.0052001592456112165 */
+        /* 3: {0.7437170678; 1.0022891615; 0.0880506421; 0.000001; 0.9924475277} => -0.004149376395503751 */
+        /* 4: {0.7291686544; 1.0205134478; 0.1663869982; 0.0594168354; 0.995573057} => -0.0014379205815923106 */
+        /* 33: {0.080287964; 1.022639996; 0.1791033455; 0.107317332; 0.9913526304} => -0.0011627986176832824 */
+        setEach(agents, Agent::setAlpha,             0.1);  //0.7291686544); //1.0335558193); //0.7437170678);
+        setEach(agents, Agent::setAlphaMax,          1e24);         
+        setEach(agents, Agent::setAlphaMultiplier,   1.02); //1.0205134478); //1.0032247029); //1.0022891615);
+        setEach(agents, Agent::setLambdaMax,         1000.0);       
+        setEach(agents, Agent::setLambdaMultiplier,  0.15); //0.1663869982); //0.0516760301); //0.0880506421);
+        setEach(agents, Agent::setEpsilon,           0.1);  //0.0594168354); //0.000001);     //0.000001);    
+        setEach(agents, Agent::setEpsilonMultiplier, 0.999);//0.995573057);  //0.9919932057); //0.999999);    
 
 		return agents;
 	}
